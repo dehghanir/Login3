254f1517376418df1343cce6269765f2801af90b
dehghanir <dehghani.r@gmail.com>
2019-04-18 19:39:35 +0430

    13980129

--- app/src/main/java/com/example/login/instaApi/Interfaces/AuthenticationListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.login.instaApi.Interfaces;
-
-public interface AuthenticationListener {
-
-    void onCodeReceived(String auth_token);
-}
--- app/src/main/java/com/example/login/instaApi/models/User.java
+++
@@ -1,4 +0,0 @@
-package com.example.login.instaApi.models;
-
-public class User {
-}
